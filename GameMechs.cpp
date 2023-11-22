573e40d1811fae780621e72436d56fa6ba1f5d27
Ryan <farekhr@mcmaster.ca>
2023-11-22 18:38:44 -0500

    Game Mechanics class updated

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -2,52 +2,84 @@
 
 GameMechs::GameMechs()
 {
+    boardSizeX = 30;
+    boardSizeY = 15;
+    exitFlag = false;
+    loseFlag = false;
+    score = 0;
+    
 
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
-
+    boardSizeX = boardX;
+    boardSizeY = boardY;
+    exitFlag = false;
+    loseFlag = false;
+    score = 0;
 }
 
 // do you need a destructor?
+GameMechs::~GameMechs()
+{
 
-
+}
 
 bool GameMechs::getExitFlagStatus()
 {
+    return exitFlag;
+}
 
+bool GameMechs::getLoseFlagStatus()
+{
+    return loseFlag;
 }
 
 char GameMechs::getInput()
 {
-
+    return input;
 }
 
 int GameMechs::getBoardSizeX()
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY()
 {
-
+    return boardSizeY;
 }
 
 
 void GameMechs::setExitTrue()
 {
+    exitFlag = true;
+}
 
+void GameMechs::setLoseFlag()
+{
+    loseFlag = true;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+    input = this_input;
 }
 
 void GameMechs::clearInput()
 {
+    input = '\0';
+}
 
+int getScore()
+{
+    return score;
+}
+
+void incrementScore()
+{
+    score++;
 }
 
 
